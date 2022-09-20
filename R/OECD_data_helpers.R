1d1752ce461b683bf3da566318d81f8b0756be0b
Nikhil Chandra <nchandra@CLOUD9.CABNET>
2022-09-20 14:21:07 +1000

    More OECD helpers

--- R/OECD_data_helpers.R
+++ R/OECD_data_helpers.R
@@ -418,6 +418,180 @@ get_oecd_labour_force_live <- function(
 
 }
 
+#' This function will look inside the 'DP_LIVE' data catalog and return a tibble
+#' of indicators that are available inside of the OECD database. 'DP_LIVE'
+#' contains various different economic, social and government indicators
+#' reported by countries to the OECD. These are not harmonized statistics,
+#' so when looking at things like GDP, CPI you will be viewing the data as
+#' reported by the Country. These figures should align with reported numbers
+#' on various FX calender websites or the relevant statistics agencies for the
+#' relevant country.
+#'
+#' @return (tibble)
+#' @export
+#'
+#' @examples \dontrun{
+#'
+#' available_indicators <- get_oecd_avlble_indctors_DPLIVE()
+#'
+#' GDP_based_indicators <- available_indicators %>%
+#'    filter(str_detect(indicator_desc, "GDP|gdp")) %>%
+#'    pull(INDICATOR)
+#'
+#' dat <- get_oecd_data_DP_LIVE(
+#'   indicators = GDP_based_indicators,
+#'    countries = c("USA"),
+#'    start_time = 2020,
+#'    end_time = 2021
+#' )
+#'
+#' }
+get_oecd_avlble_indctors_DPLIVE <- function(){
+
+  dstruc <- suppressMessages(get_dstrucs("DP_LIVE"))
+
+
+  indicator_details <- dstruc$INDICATOR %>%
+    dplyr::rename(INDICATOR = .data$id,
+                  indicator_desc = .data$label)
+
+  return(indicator_details)
+
+}
+
+
+#' This function will look inside the 'DP_LIVE' data catalog and return a tibble
+#' of data for the indicators chosen by the user.
+#' Use `get_oecd_avlble_indctors_DPLIVE` to find out what is available. 'DP_LIVE'
+#' contains various different economic, social and government indicators
+#' reported by countries to the OECD. These are not harmonized statistics,
+#' so when looking at things like GDP, CPI you will be viewing the data as
+#' reported by the Country. These figures should align with reported numbers
+#' on various FX calender websites or the relevant statistics agencies for the
+#' relevant country.
+#'
+#' @param indicators (character vector) A vector of indicators requested by the
+#' user. Example: c("EMP", "G_EMP", "LF") this would return various employment
+#' indicators.
+#' @param countries (character) Country abbreviations used by the function to
+#' determine which countries will be extracted. Use `get_oecd_country_abbrevs`
+#' to get the different abbreviations and associated country names.
+#' @param start_time (numeric) The earliest year you want to go back to. The
+#' function will attempt to go back as far as it can for the countries chosen.
+#' @param end_time (numeric) Latest date you wish to extract data for.
+#' @param remove_abbrev_cols (Boolean; TRUE) Will remove unnecessary abbreviation
+#' columns if set to TRUE.
+#'
+#' @return (tibble)
+#' @export
+#'
+#' @examples \dontrun{
+#'
+#' available_indicators <- get_oecd_avlble_indctors_DPLIVE()
+#'
+#' GDP_based_indicators <- available_indicators %>%
+#'    filter(str_detect(indicator_desc, "GDP|gdp")) %>%
+#'    pull(INDICATOR)
+#'
+#' dat <- get_oecd_data_DP_LIVE(
+#'   indicators = GDP_based_indicators,
+#'    countries = c("USA"),
+#'    start_time = 2020,
+#'    end_time = 2021
+#' )
+#'
+#' }
+get_oecd_data_DP_LIVE <- function(
+  indicators = c("EMP", "G_EMP", "LF",
+                 "G_UNEMP", "HUR", "LTUNEMP",
+                 "LFPR", "QGDP", "HHSAV"),
+  countries = c("USA", "GBR"),
+  start_time = 2020,
+  end_time = 2022,
+  remove_abbrev_cols = TRUE
+){
+
+  dstruc <- suppressMessages(get_dstrucs("DP_LIVE"))
+
+  country_list <- dstruc$LOCATION
+
+  measure_details <- dstruc$MEASURE %>%
+    dplyr::rename(MEASURE = .data$id,
+                  measure_desc = .data$label)
+
+  series_details <- dstruc$SUBJECT %>%
+    dplyr::rename(SUBJECT = .data$id,
+                  subject_desc = .data$label)
+
+  indicator_details <- dstruc$INDICATOR %>%
+    dplyr::rename(INDICATOR = .data$id,
+                  indicator_desc = .data$label)
+
+  full_country <- dstruc$LOCATION %>%
+    dplyr::rename(LOCATION = .data$id,
+                  country = .data$label)
+
+  time_info <- dstruc$TIME_FORMAT %>%
+    dplyr::rename(TIME_FORMAT = .data$id,
+                  time_details = .data$label)
+
+  frequency_info <- dstruc$FREQUENCY %>%
+    dplyr::rename(FREQUENCY = .data$id,
+                  frequency = .data$label)
+
+  dat <- OECD::get_dataset(dataset = "DP_LIVE",
+                               filter = list(countries, indicators),
+                               start_time = start_time,
+                               endtime = end_time) %>%
+    oecd_rename_obsTime()
+
+  dat_filter <- dat %>%
+    dplyr::mutate(year = stringr::str_extract(.data$Time,"[0-9][0-9][0-9][0-9]")) %>%
+    dplyr::mutate(quarter_date =
+                    dplyr::case_when(
+                      stringr::str_detect(.data$Time,"Q1") ~ glue::glue("{year}-03-01"),
+                      stringr::str_detect(.data$Time,"Q2") ~ glue::glue("{year}-06-01"),
+                      stringr::str_detect(.data$Time,"Q3") ~ glue::glue("{year}-09-01"),
+                      stringr::str_detect(.data$Time,"Q4") ~ glue::glue("{year}-12-01")
+                    )
+    ) %>%
+    dplyr::mutate(
+      date = lubridate::as_date(.data$Time)
+    ) %>%
+    dplyr::left_join(
+      full_country, by = "LOCATION"
+    ) %>%
+    dplyr::left_join(
+      time_info, by = "TIME_FORMAT"
+    )  %>%
+    dplyr::left_join(
+      series_details, by = "SUBJECT"
+    )  %>%
+    dplyr::left_join(
+      indicator_details, by = "INDICATOR"
+    ) %>%
+    dplyr::left_join(
+      measure_details, by = "MEASURE"
+    ) %>%
+    dplyr::left_join(
+      frequency_info, by = "FREQUENCY"
+    ) %>%
+    dplyr::mutate(
+      date = ifelse(!is.na(.data$quarter_date) & is.na(.data$date),
+                    .data$quarter_date, .data$date)
+    )
+
+  if(remove_abbrev_cols){
+    dat_filter <- dat_filter %>%
+      dplyr::select(-.data$MEASURE, -.data$TIME_FORMAT, -.data$INDICATOR,
+                    -.data$SUBJECT, -.data$Time,
+                    -.data$FREQUENCY)
+  }
+
+  return(dat_filter)
+
+}
+
 oecd_rename_obsTime <- function(.data){
 
   dat_names <- names(.data)
